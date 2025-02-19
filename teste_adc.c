4e6034fd03d8c038478cfd911415f974c9aab16a
Felpzs0206 <felipepsilva0206@gmail.com>
2025-02-18 21:49:25 -0300

    comentarios + mudanças

--- teste_adc.c
+++ teste_adc.c
@@ -5,15 +5,16 @@
 #include "hardware/pwm.h"
 #include "lib/ssd1306.h"
 
-#define VRX_PIN 26
-#define VRY_PIN 27
-#define SW_PIN 22
+#define VRX_PIN 26 // Pino X joystick
+#define VRY_PIN 27 // Pino Y joystick
+#define SW_PIN 22 // Pino do botão do joystick
 #define LED_RED_PIN 13  // Pino do LED Vermelho (eixo X)
 #define LED_BLUE_PIN 12 // Pino do LED Azul (eixo Y)
 #define LED_GREEN_PIN 11  // Pino do LED Verde
-#define BUTTON_A_PIN 5
+#define BUTTON_A_PIN 5 // Pino do botão A
 #define BUTTON_JOYSTICK_PIN 22
-#define I2C_SDA 14
+
+#define I2C_SDA 14 
 #define I2C_SCL 15
 #define I2C_PORT i2c1
 #define endereco 0x3C
@@ -74,7 +75,7 @@ void button_callback(uint gpio, uint32_t events) {
     uint32_t now = to_ms_since_boot(get_absolute_time());
 
     // Debounce para o botão A
-    if (gpio == BUTTON_JOYSTICK_PIN && (now - last_button_a_time) > debounce_time_ms) {
+    if (gpio == SW_PIN && (now - last_button_a_time) > debounce_time_ms) {
         gpio_put(LED_GREEN_PIN, !gpio_get(LED_GREEN_PIN));  // Alterna o estado do LED Verde
         current_border = !current_border; // Alterna a borda do display
         last_button_a_time = now;// Atualiza o tempo do último clique
@@ -139,13 +140,13 @@ int main()
     gpio_pull_up(BUTTON_A_PIN); // Habilita o pull-up interno
 
     // Configuração do Botão do Joystick
-    gpio_init(BUTTON_JOYSTICK_PIN); // Inicializa o pino do botão do joystick
-    gpio_set_dir(BUTTON_JOYSTICK_PIN, GPIO_IN); // Configura o pino do botão do joystick como entrada
-    gpio_pull_up(BUTTON_JOYSTICK_PIN); // Habilita o pull-up interno
+    gpio_init(SW_PIN); // Inicializa o pino do botão do joystick
+    gpio_set_dir(SW_PIN, GPIO_IN); // Configura o pino do botão do joystick como entrada
+    gpio_pull_up(SW_PIN); // Habilita o pull-up interno
 
     
     gpio_set_irq_enabled_with_callback(BUTTON_A_PIN, GPIO_IRQ_EDGE_FALL, true, &button_callback); // Habilita a interrupção para o botão A
-    gpio_set_irq_enabled_with_callback(BUTTON_JOYSTICK_PIN, GPIO_IRQ_EDGE_FALL, true, &button_callback); // Habilita a interrupção para o botão do joystick
+    gpio_set_irq_enabled_with_callback(SW_PIN, GPIO_IRQ_EDGE_FALL, true, &button_callback); // Habilita a interrupção para o botão do joystick
     
 
     while(1){
@@ -181,7 +182,7 @@ int main()
         
         }
 
-        sleep_ms(100);  // Aguarda 100ms antes de ler novamente
+        sleep_ms(50);  // Aguarda 100ms antes de ler novamente
     }
 
     return 0;
